1c5fcd46c55ad2f948f322d78b66bdb7bdd51bfc
DoraKK <131965929+komAAmok@users.noreply.github.com>
2026-06-07 11:00:40 +0800

    Update setup.py
    移除 has_ext_modules 声明，兼容所有python3

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 # setup.py
 
-
 from setuptools import setup
 from setuptools.dist import Distribution
 
 class BinaryDistribution(Distribution):
-    def has_ext_modules(foo):
-        return True
-
     def is_pure(foo):
         return False
 
-# 仅传递 distclass，其他所有配置都会自动从 pyproject.toml 中读取并合并
 setup(
     distclass=BinaryDistribution,
 )
